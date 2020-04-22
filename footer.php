1b92a70aba836cea5699420e8cbae6defac13693
Ken Grier <grierkag@gmail.com>
2020-04-21 22:29:00 -0400

    updates

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer>
+    <section class="container">
+        <section class="row">
+            <div class="col">
+                <h2 class="contact-title">Contact</h2>
+                <p class="lg-body">
+                    Feeling inspired? Want to tell me something sucks? 
+                    Reach out to me at <a href="mailtto">grierkag@gmail.com</a>
+                </p>
+            </div>
+            <div class="col">
+                <div class="spinner">
+                <!------- SPINNER GOES HERE ---------->
+                </div>
+                <div class="social">
+                    <ul class="md-text-right">
+                        <li><a href="#">instagram</a></li>
+                        <li><a href="#">dribbble</a></li>
+                        <li><a href="#">twitter</a></li>
+                        <li><a href="#">behance</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </section>
+</footer>
